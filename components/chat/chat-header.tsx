42a61c090b0ae8e2b05718fa5e61288e0f39772c
fkiptooh <fkiptooh@gmail.com>
2023-10-19 18:52:45 +0300

    feat(fetch/create conversation) - util function

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,5 +1,6 @@
 import { Hash } from "lucide-react";
 import { MobileToggle } from "../mobile-toggle";
+import { UserAvatar } from "../user-avatar";
 
 interface ChartHearderProps {
   serverId: string;
@@ -19,6 +20,9 @@ export const ChatHeader = ({
       <MobileToggle serverId={serverId} />
       {type === "channel" && (
         <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />
+      )}
+      {type === "conversation" && (
+        <UserAvatar className="h-8 w-8 md:h-8 md:w-8 mr-4" src={imageUrl} />
       )}
       <p className="text-semibold text-md text-black dark:text-white">{name}</p>
     </div>
